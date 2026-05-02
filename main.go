6d1d1e8c18e5c77dbd33c4ba391fdcac1fd6f3d8
ayushmanmishra18 <ayushmanmishraji1@gmail.com>
2026-05-02 21:03:46 +0530

    url shorten redirect code done

--- main.go
+++ main.go
@@ -24,6 +24,49 @@ func main() {
 	app := fiber.New()
 
 	//health check endpoint
-	app.Get("/health", func(c *fiber.Ctx) error {
-		return c.SendString("OK")
+	app.Get("/health",func(c *fiber.Ctx)error {
+		return c.JSON(fiber.Map{
+			"message":"Server is runnning",
+		})
 	})
+
+	//shorten url
+
+	app.Post("/shorten",func(c *fiber.Ctx)error {
+		type Request struct {
+			URL string `json:"url"`
+		}
+
+		
+		var body Request
+		if err := c.BodyParser(&body); err != nil || body.URL == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Invalid request",
+			})
+		}
+
+		code := generateCode(6)
+		urlStore[code] = body.URL
+
+		return c.JSON(fiber.Map{
+			"short_url": "http://localhost:3000/" + code,
+		})
+	})
+
+ //redirect 
+// redirect
+	app.Get("/:code", func(c *fiber.Ctx) error {
+		code := c.Params("code")
+
+		url, exists := urlStore[code]
+		if !exists {
+			return c.Status(404).JSON(fiber.Map{
+				"error": "URL not found",
+			})
+		}
+
+		return c.Redirect(url, 302)
+	})
+
+	app.Listen(":3000")
+}
