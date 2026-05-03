310e2b6d3fbf2af06a709de0af19a4bcda0bb8e7
ayushmanmishra18 <ayushmanmishraji1@gmail.com>
2026-05-03 15:43:15 +0530

    added safety for counter,url validation and base 62 logic of encoding

--- main.go
+++ main.go
@@ -1,45 +1,60 @@
 package main
 
 import (
-	"math/rand"
-	"time"
+	"net/url"
+	"sync"
+
 	"github.com/gofiber/fiber/v2"
-
 )
-var urlStore=make(map[string]string)
 
+var urlStore = make(map[string]string)
 
-var counter int64=1
-//generate random short code 
+// mutex for thread-safe counter
 
-func generateCode(n int )string{
-	const letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	rand.Seed(time.Now().UnixNano())
-	code := make([]byte, n)
-	for i := 0; i < n; i++ {
-		code[i] = letters[rand.Intn(len(letters))]
+var mu sync.Mutex
+var counter int64 = 1
+
+const base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// Base62 encoder to generate short codes
+func encodeBase62(num int64) string {
+	if num == 0 {
+		return string(base62Chars[0])
 	}
-	return string(code)
+
+	var encoded []byte
+
+	for num > 0 {
+		remainder := num % 62
+		encoded = append([]byte{base62Chars[remainder]}, encoded...)
+		num = num / 62
+	}
+
+	return string(encoded)
+}
+
+// validate URL
+func isValidURL(input string) bool {
+	u, err := url.ParseRequestURI(input)
+	return err == nil && u.Scheme != "" && u.Host != ""
 }
 
 func main() {
 	app := fiber.New()
 
-	//health check endpoint
-	app.Get("/health",func(c *fiber.Ctx)error {
+	// health check
+	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"message":"Server is runnning",
+			"message": "Server is running",
 		})
 	})
 
-	//shorten url
-
-	app.Post("/shorten",func(c *fiber.Ctx)error {
+	// shorten URL
+	app.Post("/shorten", func(c *fiber.Ctx) error {
 		type Request struct {
 			URL string `json:"url"`
 		}
 
-		
 		var body Request
 		if err := c.BodyParser(&body); err != nil || body.URL == "" {
 			return c.Status(400).JSON(fiber.Map{
@@ -47,7 +62,19 @@ func main() {
 			})
 		}
 
-		code := generateCode(6)
+		// validate URL
+		if !isValidURL(body.URL) {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Invalid URL format",
+			})
+		}
+
+		// thread-safe counter increment
+		mu.Lock()
+		code := encodeBase62(counter)
+		counter++
+		mu.Unlock()
+
 		urlStore[code] = body.URL
 
 		return c.JSON(fiber.Map{
@@ -55,8 +82,7 @@ func main() {
 		})
 	})
 
- //redirect 
-// redirect
+	// redirect
 	app.Get("/:code", func(c *fiber.Ctx) error {
 		code := c.Params("code")
 
